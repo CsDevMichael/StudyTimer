5638c0ae672f58c43079201244da3b9726e16e06
John Michael <michael03.csdev@gmail.com>
2026-09-21 14:53:37 +0530

    Create JavaFX desktop window

--- src/main/java/com/study/timer/App.java
+++ src/main/java/com/study/timer/App.java
@@ -1,10 +1,25 @@
 package com.study.timer;
 
-/**
- * Hello world!
- */
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label title = new Label("StudyTimer");
+
+        Scene scene = new Scene(title, 400, 250);
+
+        stage.setTitle("StudyTimer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
     }
 }
